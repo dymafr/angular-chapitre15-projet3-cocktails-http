ccd81989f02ddf7b883120641cf5444254e3d7ef
quentin <quentin@dyma.fr>
2024-12-14 12:44:56 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer.component';
+import { CocktailsComponent } from './components/cocktails/cocktails.component';
+// import { seedData } from './shared/data/seed';
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent, FooterComponent, CocktailsComponent],
+  template: `
+    <app-header />
+    <app-cocktails class="flex-auto" />
+    <app-footer />
+  `,
+  styles: `
+    :host {
+      min-height: 100vh;
+      display:flex;
+      flex-direction:column;
+    }
+ `,
+})
+export class AppComponent {
+  constructor() {
+    // seedData();
+  }
+}
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { HeaderMenuComponent } from './components/header-menu.component';
+
+@Component({
+  selector: 'app-header',
+  imports: [HeaderMenuComponent],
+  template: `
+    <div class="flex-auto text-bold text-lg">Cocktails</div>
+    <ul class="xs-hide flex flex-row gap-16">
+      <li>
+        <a href="#">Liste des cocktails</a>
+      </li>
+      <li>
+        <a href="#">Panier</a>
+      </li>
+    </ul>
+    <app-header-menu class=" hide xs-show  " />
+  `,
+  styles: `
+    :host {
+      position: relative;
+      display: flex;
+      align-items:center;
+      background-color: var(--primary);
+      color: white;
+      height: 56px;
+      padding: 0 16px;
+    }
+  `,
+})
+export class HeaderComponent {}
